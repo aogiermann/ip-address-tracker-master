1bd529c9fef26c5d2427f9b79b08b08b275dba38
Adam Ogiermann <aogiermann@gmail.com>
2020-09-11 19:19:40 +0200

    added vercel serverless function

---
+++ api/getCoordinates.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+module.exports = (req, res) => {
+    let regEx = `\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
+    (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
+    (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
+    (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b`;
+
+    let apiQuery = `https://geo.ipify.org/api/v1?apiKey=${process.env.IPYFIKEY}&ipAddress=`;
+
+    if(regEx.test($query)) {
+        apiQuery = apiQuery + $query;
+    }
+    else {
+        apiQuery = apiQuery + `8.8.8.8&domain=${$query}`;
+    }
+
+    const result = await fetch(apiQuery);
+
+    res.status(200).json(await result.json);
+  }
