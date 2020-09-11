dbf454b039b461ec3ffd8527d5a23cafe0bbb18d
Adam Ogiermann <aogiermann@gmail.com>
2020-09-11 19:33:33 +0200

    serversell funciton - different approach

--- api/getCoordinates.js
+++ api/getCoordinates.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
     let regEx = `\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
     (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
     (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.
@@ -15,7 +15,5 @@ module.exports = async (req, res) => {
         apiQuery = apiQuery + `8.8.8.8&domain=${$query}`;
     }
 
-    const result = await fetch(apiQuery);
-
-    res.status(200).json(await result.json);
+    res.redirect(apiQuery);
   }
